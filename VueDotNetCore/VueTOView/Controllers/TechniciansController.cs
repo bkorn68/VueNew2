6991c4088a1c7270e25a7278a8f3dd130cbf8b3f
Bernhard <b.korn@boservice.de>
2020-10-13 15:53:11 +0200

    VueTOView TechniciansController added

---
+++ VueDotNetCore/VueTOView/Controllers/TechniciansController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceReference2;
+using VueTOView.Common;
+using VueTOView.Converters;
+using VueTOView.RequestData;
+using VueTOView.ResponseData;
+
+namespace VueTOView.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TechniciansController : ControllerBase
+    {
+        [HttpPost]
+        public IEnumerable<GetTechniciansResponseData> GetTechnicians([FromBody]GetTechniciansRequest getTechniciansRequest)
+        {
+            List<GetTechniciansResponseData> result = null;
+            List<Technician> technicianlist = GetTechniciansFromService(getTechniciansRequest);
+            result = technicianlist.Select(e => TechnicianToResponseDataConverter.Convert(e)).ToList();
+            return result;
+        }
+
+        private  List<Technician> GetTechniciansFromService(GetTechniciansRequest getTechniciansRequest)
+        {
+            List<Technician> tourtechnicians = new List<Technician>();
+            WebToolExtendedServiceClient client = ClientProvider.ProvideTOClient(getTechniciansRequest.environment);
+            Task<ServiceReference2.ResultListOfTechnicianuukIAVwv> task = client.GetTechniciansAsync(getTechniciansRequest.ident, getTechniciansRequest.mandatorId, null);
+            ResultListOfTechnicianuukIAVwv result = task.Result;
+            if ((result != null) && (string.IsNullOrEmpty(result.Error)) && (result.DataList != null) && (result.DataList.Length > 0))
+            {
+                List<Technician> technicians = result.DataList.ToList();
+                foreach (Technician tec in technicians.Where(x => !x.IsSubcontractor).OrderBy(x => x.Name))
+                {
+                    tourtechnicians.Add(tec);
+                }
+
+            }
+            return tourtechnicians;
+
+
+
+        }
+
+
+    }
+}
---
+++ VueDotNetCore/VueTOView/Converters/TechnicianToResponseDataConverter.cs
@@ -0,0 +1,23 @@
+﻿using ServiceReference2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VueTOView.ResponseData;
+
+namespace VueTOView.Converters
+{
+    public static class TechnicianToResponseDataConverter
+    {
+        public static GetTechniciansResponseData Convert(Technician source)
+        {
+            GetTechniciansResponseData target = new GetTechniciansResponseData()
+            {
+                Name = source.Name,
+                SourceId = source.ID.SourceId
+            };
+            return target;
+        }
+
+    }
+}
